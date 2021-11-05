a6090c55700313b7175363da48eef7a34091691a
Hovhannes Babayan <hbabayan@adobe.com>
2021-11-05 20:30:05 +0400

    fix: make code work as a zsh alias

--- git_clone_into_local_path.sh
+++ git_clone_into_local_path.sh
@@ -1,65 +1,65 @@
 # Clones given git url to a predefined folder in your machine.
 #
 # Usage:
-#   clone git@gitlab.workfront.tech:visibility/titan-solr-search.git
+#   ./git_clone_into_local_path.sh git@gitlab.workfront.tech:visibility/titan-solr-search.git
 # 
-# If you don't want to open editor autmatically, pass --no-open flag:
-#   clone --no-open git@gitlab.workfront.tech:visibility/titan-solr-search.git
+# If you don't want to open editor automatically, pass --no-open flag:
+#   ./git_clone_into_local_path.sh --no-open git@gitlab.workfront.tech:visibility/titan-solr-search.git
 # 
 # Links from github.com are also supported:
-#   clone git@github.com:bvaughn/react-window.git
+#   ./git_clone_into_local_path.sh git@github.com:bvaughn/react-window.git
 #
-git_clone_into_local_path() {
-    set -euo pipefail
-    local git_extension=".git"
-    
-    # Which editor to use
-    local editor="webstorm"   
-    
-    # Where to clone projects from work gitlab instance
-    local local_work_gitlab_dir="${HOME}/dev/gitlab/"
+# As zsh alias:
+#   alias clone="~/Projects/github/bhovhannes/scripts/git_clone_into_local_path.sh"
+#
 
-    # Where to clone projects from github.com
-    local local_personal_github_dir="${HOME}/Projects/github/"
+set -euo pipefail
+_git_extension=".git"
 
-    # loop through command-line args and fill local variables accordingly
-    local open=1
-    local url
-    while [[ "$#" -gt 0 ]]; do
-        case $1 in
-            --no-open) open=0 ;;
-            *) url=$1 ;;
-        esac
-        shift
-    done
+# Which editor to use
+_editor="webstorm"   
 
-    if [[ $url == git@* ]]
+# Where to clone projects from work gitlab instance
+local_work_gitlab_dir="${HOME}/dev/gitlab/"
+
+# Where to clone projects from github.com
+local_personal_github_dir="${HOME}/Projects/github/"
+
+# loop through command-line args and fill variables accordingly
+_open=1
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --no-_open) _open=0 ;;
+        *) _url=$1 ;;
+    esac
+    shift
+done
+
+if [[ $_url == git@* ]]
+then
+    # Determine _path from git _url
+    _path=${_url#*@}
+    _host=${_path%:*}
+    _path=${_path#*:}
+    if [[ $_path == *${_git_extension} ]]
     then
-        # Determine path from git url
-        local path=${url#*@}
-        local host=${path%:*}
-        path=${path#*:}
-        if [[ $path == *${git_extension} ]]
-        then
-            path=${path%${git_extension}}
-        fi
-
-        # Build local directory path to clone repo into
-        case $host in
-        "gitlab.workfront.tech") path="${local_work_gitlab_dir}${path}" ;;
-        "github.com") path="${local_personal_github_dir}${path}" ;;
-        esac
-
-        echo "Cloning ${url} ..."
-        git clone "${url}" "${path}"
-        
-        if [[ $open == 1 ]]
-        then
-            echo "Opening ${path} in editor ..."
-            command $editor "${path}"
-        fi
-    else
-        echo "Don't know what to do with ${url}. Please pass a valid git url, starting with 'git@'."
-        return 1
+        _path=${_path%${_git_extension}}
     fi
-}
+
+    # Build directory _path to clone repo into
+    case $_host in
+    "gitlab.workfront.tech") _path="${local_work_gitlab_dir}${_path}" ;;
+    "github.com") _path="${local_personal_github_dir}${_path}" ;;
+    esac
+
+    echo "Cloning ${_url} ..."
+    git clone "${_url}" "${_path}"
+    
+    if [[ $_open == 1 ]]
+    then
+        echo "Opening ${_path} in editor ..."
+        (exec $_editor "${_path}")
+    fi
+else
+    echo "Don't know what to do with ${_url}. Please pass a valid git url, starting with 'git@'."
+fi
